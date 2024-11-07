72621a445c76bef27694a488f9650f0f6762a090
lirovsk <joao.lira@engenharia.ufjf.br>
2024-11-07 18:29:01 -0300

    working with variables

---
+++ manipulating_variabels.py
@@ -0,0 +1,32 @@
+# setting the variables
+my_age = 20
+my_name = "joao"
+my_height = 1.80
+ 
+# printing the variables
+print(my_age)
+print(my_name)
+print(my_height)
+print("\n")
+
+# changing the types: int to float and int to string
+print(float(my_age))
+print(type(float(my_age)))
+
+print(str(my_age))
+print(type(str(my_age)))
+print("\n")
+
+#changing the types: float to int and float to string
+print(int(my_height))
+print(type(int(my_height)))
+
+print(str(my_height))
+print(type(str(my_height)))
+print("\n")
+
+#changing the types: int to float using division
+print(my_age/2)
+print(type(my_age/2))
+
+#It is not possible to change the type of string within alphanumeric characters on it to float or int
