a99f04cb3c55db33dd670140eec2e7f0f87a98df
Lirovsk <joao.lira@engenharia.ufjf.br>
2024-11-21 18:31:41 -0300

    Tryin to make this aplication more user-friedly with some strings methods

--- Working_w_operators_structures.py
+++ Working_w_operators_structures.py
@@ -1,5 +1,6 @@
 #in this file I'm amimng to work with operators in python, so, in somtimes, I won't use the sortest way to solve a problem.
 import time as t
+import sys
 #Variable Declaration
 my_name = "Joao"
 my_weight = 80
@@ -21,78 +22,151 @@ else:
 
 if im_hungry:
     time_now = int(input("What time is it now? "))
-else: print("It is okay then")
-
-if time_now >6 and time_now <9:
-    time_eat = "breakfast"
-    print("It is breakfast time!")
-    t.sleep(1)
-    for i in range(0,4): 
-        if inside_fridge[2][i] == time_eat:
-            output_user[2][i] = True
-    print("In the frezer we have ")
-    for i in range (0,4):
-        if output_user[2][i] == True:
-            print( inside_fridge[0][i])
-    print ("enjoy!")               
-
-elif time_now >= 9 and time_now < 11:
-    print("It is morning break time!")
-    t.sleep(1)
-    for i in range(0,4): 
-        if "morning break" in inside_fridge[2][i]:
-            output_user[2][i] = True
-    print("In the frezer, for morning break, we have ")
-    for i in range (0,4):
-        if output_user[2][i] == True:
+    if time_now >6 and time_now <9:
+        time_eat = "breakfast"
+        print("It is breakfast time!")
+        t.sleep(0.7)
+        for i in range(0,4): 
+            if inside_fridge[2][i] == time_eat:
+                output_user[2][i] = True
+        print("In the frezer we have ")
+        for i in range (0,4):
+            if output_user[2][i] == True:
                 print( inside_fridge[0][i])
-    print ("but you can also eat ")
-    for i in range (0,4):
-        if (output_user[2][i] == False) and ("break" in inside_fridge[2][i]):
-            print( inside_fridge[0][i])
+                sys.stdout.flush()
+        t.sleep(0.4)
+        print(" Thanks for using this program ".center(70, "#"))
+        print("Exiting", end = "")
+        sys.stdout.flush()
+        for i in range(0,3):
+            print(".", end= "")
+            sys.stdout.flush()
+            t.sleep(0.5)              
 
-elif time_now >= 12 and time_now < 14:
-    print("It is lunch time!")
-    t.sleep(1)
-    for i in range (0,4):
-        if "lunch" in inside_fridge[2][i]:
-            output_user[2][i] = True
-    print("In the frezer, for lunch, we have ", end = "")
-    for i in range (0,4):
-        if output_user[2][i] == True:
-            print( inside_fridge[0][i])
-
-elif time_now >= 14 and time_now < 18:
-    print("It is fternoon break time!")
-    t.sleep(1)
-    for i in range(0,4): 
-        if "fternoon break" in inside_fridge[2][i]:
-            output_user[2][i] = True
-    print("In the frezer, for fternoon break, we have ", end = "")
-    for i in range (0,4):
-        if output_user[2][i] == True:
-            print( inside_fridge[0][i])
-    print ("but you can also eat ")
-    for i in range (0,4):
+    elif time_now >= 9 and time_now < 11:
+        print("It is morning break time!")
+        t.sleep(0.7)
+        for i in range(0,4): 
+            if "morning break" in inside_fridge[2][i]:
+                output_user[2][i] = True
+        print("In the frezer, for morning break, we have: ", end= "")
+        for i in range (0,4):
+            if output_user[2][i] == True:
+                    print( inside_fridge[0][i])
+                    sys.stdout.flush()
+        t.sleep(0.5)
+        print ("but you can also eat ")
+        for i in range (0,4):
             if (output_user[2][i] == False) and ("break" in inside_fridge[2][i]):
                 print( inside_fridge[0][i])
+                sys.stdout.flush()
+        t.sleep(0.4)
+        print(" Thanks for using this program ".center(70, "#"))
+        print("Exiting", end = "")
+        sys.stdout.flush()
+        for i in range(0,3):
+            print(".", end= "")
+            sys.stdout.flush()
+            t.sleep(0.5)
 
-elif time_now >= 19 and time_now < 22:
-    time_eat = "dinner"
-    print("It is dinner time!")
-    t.sleep(1)
-    for i in range (0,4):
-        if "dinner" in inside_fridge[2][i]:
-            output_user[2][i] = True
-    print("In the frezer, for dinner, we have ", end = "")
-    for i in range (0,4):
-        if output_user[2][i] == True:
-            print( inside_fridge[0][i])
+    elif time_now >= 12 and time_now < 14:
+        print("It is lunch time!")
+        t.sleep(0.7)
+        for i in range (0,4):
+            if "lunch" in inside_fridge[2][i]:
+                output_user[2][i] = True
+        print("In the frezer, for lunch, we have: ", end = "")
+        for i in range (0,4):
+            if output_user[2][i] == True:
+                print( inside_fridge[0][i])
+                sys.stdout.flush() 
+        t.sleep(0.4)
+        print(" Thanks for using this program ".center(70, "#"))
+        print("Exiting", end = "")
+        sys.stdout.flush()
+        for i in range(0,3):
+            print(".", end= "")
+            sys.stdout.flush()
+            t.sleep(0.5)
 
-elif time_now==11 or time_now==18:
-    t.sleep(1)
-    print("It is too close to the next meal, you should wait a little bit more")
+    elif time_now >= 14 and time_now < 18:
+        print("It is fternoon break time!")
+        t.sleep(0.7)
+        for i in range(0,4): 
+            if "fternoon break" in inside_fridge[2][i]:
+                output_user[2][i] = True
+        print("In the frezer, for fternoon break, we have: ", end = "")
+        for i in range (0,4):
+            if output_user[2][i] == True:
+                print( inside_fridge[0][i])
+                sys.stdout.flush()
+        t.sleep(0.5)
+        print ("but you can also eat ")
+        for i in range (0,4):
+                if (output_user[2][i] == False) and ("break" in inside_fridge[2][i]):
+                    print( inside_fridge[0][i])
+                    sys.stdout.flush()
+        t.sleep(0.4)
+        print(" Thanks for using this program ".center(70, "#"))
+        print("Exiting", end = "")
+        sys.stdout.flush()
+        for i in range(0,3):
+            print(".", end= "")
+            sys.stdout.flush()
+            t.sleep(0.5)
 
-else:
-    t.sleep(1)
-    print("You should not eat now, go to sleep")
+    elif time_now >= 19 and time_now < 22:
+        time_eat = "dinner"
+        print("It is dinner time!")
+        t.sleep(1)
+        for i in range (0,4):
+            if "dinner" in inside_fridge[2][i]:
+                output_user[2][i] = True
+        print("In the frezer, for dinner, we have: ", end = "")
+        for i in range (0,4):
+            if output_user[2][i] == True:
+                print( inside_fridge[0][i])
+                sys.stdout.flush()
+        t.sleep(0.4)
+        print(" Thanks for using this program ".center(70, "#"))
+        print("Exiting", end = "")
+        sys.stdout.flush()
+        for i in range(0,3):
+            print(".", end= "")
+            sys.stdout.flush()
+            t.sleep(0.5)
+
+    elif time_now==11 or time_now==18:
+        t.sleep(0.4)
+        print("It is too close to the next meal, you should wait a little bit more")
+        t.sleep(0.4)
+        print(" Thanks for using this program ".center(70, "#"))
+        print("Exiting", end = "")
+        sys.stdout.flush()
+        for i in range(0,3):
+            print(".", end= "")
+            sys.stdout.flush()
+            t.sleep(0.5)
+
+    else:
+        t.sleep(0.4)
+        print("You should not eat now, go to sleep")
+        t.sleep(0.4)
+        print(" Thanks for using this program ".center(70, "#"))
+        print("Exiting", end = "")
+        sys.stdout.flush()
+        for i in range(0,3):
+            print(".", end= "")
+            sys.stdout.flush()
+            t.sleep(0.5)
+else: 
+    t.sleep(0.4)
+    print("It is okay then")
+    t.sleep(0.4)
+    print(" Thanks for using this program ".center(70, "#"))
+    print("Exiting", end = "")
+    sys.stdout.flush()
+    for i in range(0,3):
+        print(".", end= "")
+        sys.stdout.flush()
+        t.sleep(0.5)
